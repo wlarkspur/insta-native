189c1319a7c42159a2a81687b35f98d846281a12
wlarkspur <newacon@naver.com>
2024-06-03 00:27:29 +0200

    native web

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import * as React from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { useAssets } from "expo-asset";
+import LoggedOutNav from "./navigators/LoggedOutNav";
+import { NavigationContainer } from "@react-navigation/native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [assets, error] = useAssets([
+    require(`./assets/Instagram-name-logo-transparent-PNG.png`),
+    "https://image.similarpng.com/very-thumbnail/2020/06/Instagram-name-logo-transparent-PNG.png",
+  ]);
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load assets, make any API calls you need to do here
+        if (assets) {
+          // Only proceed when assets are loaded
+          await SplashScreen.hideAsync();
+        }
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setLoading(false); // Set loading to false when assets are ready
+      }
+    }
+    prepare();
+  }, [assets]);
+
+  if (loading || !assets) {
+    return null; // Show nothing while loading
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigators/LoggedOutNav.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Welcome from "../screens/Welcome";
+import CreateAccount from "../screens/CreateAccount";
+
+import { NavigationContainer } from "@react-navigation/native";
+import LogIn from "../screens/LogIn";
+
+const Stack = createNativeStackNavigator();
+
+export default function LoggedOutNav() {
+  return (
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Screen name="CreateAccount" component={CreateAccount} />
+    </Stack.Navigator>
+  );
+}
